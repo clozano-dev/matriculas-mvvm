7d56883c4dde9517142a94778e7cca71ab0fece3
clozano-dev <lozanoc@protonmail.com>
2024-07-20 07:20:44 +0200

    remote database configuration

---
+++ app/src/main/java/com/clozanodev/matriculas/data/repository/PlateRepository.kt
@@ -0,0 +1,19 @@
+package com.clozanodev.matriculas.data.repository
+
+import com.clozanodev.matriculas.data.local.dao.UserStatsDao
+import com.clozanodev.matriculas.data.local.entities.UserStats
+import com.clozanodev.matriculas.data.remote.api.LicensePlateApi
+import com.clozanodev.matriculas.data.remote.entities.LicensePlate
+import javax.inject.Inject
+
+class PlateRepository @Inject constructor(
+    private val licensePlateApi: LicensePlateApi,
+    private val userStatsDao: UserStatsDao
+) {
+    suspend fun insertUserStats(userStats: UserStats) = userStatsDao.insertUserStats(userStats)
+    suspend fun getUserStats(username: String) = userStatsDao.getUserStats(username)
+
+    suspend fun getLicensePlate(id: Int): LicensePlate? = licensePlateApi.getPlate(id)
+    suspend fun getAllLicensePlates(): List<LicensePlate> = licensePlateApi.getAllPlates()
+
+}
