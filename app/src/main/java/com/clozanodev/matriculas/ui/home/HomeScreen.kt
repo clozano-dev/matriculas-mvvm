bbe7fe3f542754eea2c6e49f34459f7a224e072b
clozano-dev <lozanoc@protonmail.com>
2024-08-22 11:20:52 +0200

    vertical scroll homeScreen

--- app/src/main/java/com/clozanodev/matriculas/ui/home/HomeScreen.kt
+++ app/src/main/java/com/clozanodev/matriculas/ui/home/HomeScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.ButtonDefaults
@@ -78,7 +80,8 @@ fun HomeScreen(viewModel: MainViewModel) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
